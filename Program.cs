1feee97ae99f63da7ae92e888358968a962655ff
sworc <lightbox@nifty.com>
2022-09-05 15:56:09 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace cs_con_stream_reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\app\workspace\csv\社員マスタ.csv";
+
+            Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+            using( var sr = new StreamReader(filepath,Encoding.GetEncoding("shift_jis")) ) {
+
+                string? line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+        }
+    }
+}
