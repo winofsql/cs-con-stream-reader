611489b87eaaa6b73a53c4fcd81b544f77e9c3fd
sworc <lightbox@nifty.com>
2022-10-10 15:50:08 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,26 @@ using System.Text;
 
 namespace cs_con_stream_reader
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            string filepath = @"C:\app\workspace\csv\社員マスタ.csv";
+      Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
-
-            using( var sr = new StreamReader(filepath,Encoding.GetEncoding("shift_jis")) ) {
-
-                string? line;
-                while ((line = sr.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-            }
-
+      string? text = "";
+      using( var sr = new StreamReader( "utf8n.csv", new UTF8Encoding() ) )
+      using( var sw1 = new StreamWriter( "sjis.csv", false, Encoding.GetEncoding("shift_jis") ) )
+      using( var sw2 = new StreamWriter( "utf8_bom.csv", false, new UTF8Encoding(true) ) )
+      using( var sw3 = new StreamWriter( "ujis.csv", false, Encoding.GetEncoding("euc-jp") ) )
+      using( var sw4 = new StreamWriter( "unicode.csv", false, new UnicodeEncoding() ) )
+      {
+        while ((text = sr.ReadLine()) != null) {
+          sw1.WriteLine( text );
+          sw2.WriteLine( text );
+          sw3.WriteLine( text );
+          sw4.WriteLine( text );
         }
+      }
     }
+  }
 }
